ce1c8b136259d2d6f7d5f5809013f64be9330d6d
Sabria-F <sjfafach@students.unwsp.edu>
2025-01-30 22:46:25 -0600

    SabriaFafachProgram3TotalPurchase

--- program_3.py
+++ program_3.py
@@ -1,8 +1,18 @@
-def calculate_total_purchase():
-    # A customer in a store is purchasing five items.  
-    # Write a program that asks for each item, 
-    # then displays the subtotal of the sale, 
-    # the amount of sales tax, and the total.  
-    # Assume the sales tax is 7 percent.
+#By: Sabria Fafach
+#Date: January 30, 2025
+#Name: program_1.py
 
-calculate_total_purchase()
+cost_item1=float(input("Enter the cost in dollars of first item:"))
+cost_item2=float(input("Enter the cost in dollars of second item:"))
+cost_item3=float(input("Enter the cost in dollars of third item:"))
+cost_item4=float(input("Enter the cost in dollars of fourth item:"))
+cost_item5=float(input("Enter the cost in dollars of fifth item:"))
+
+subtotal=cost_item1+cost_item2+cost_item3+cost_item4+cost_item5
+print(f"Your subtotal is: ${subtotal}")
+
+sales_tax=0.07
+tax_amount=subtotal*sales_tax
+
+print(f"The amount of sales tax is ${tax_amount}")
+print(f"Your total is ${subtotal+tax_amount}")
